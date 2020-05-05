3a7346fcded26748c8ef2cb4ff4ef81520b1fe33
Keiran Carpen <keirancarpen@gmail.com>
2020-05-05 11:15:03 -0400

    changed to bst-traversal, install more packages, created run, dev, build scripts in package.json

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-## sorting-algorithm-visualizer
+## BST-traversal-visualizer
 
 # Codesmith solo project --
 
-In this solo project, I hope to create a graphical interface where the user can visual commonoly used sorting algorithms. The user will be able to input their own array, use a built in sorting algorithm, and visualize the array sorting process.
+In this solo project, I hope to create a graphical interface where the user can visualize BST traversal algorithms. The user will be able to input their own BST or generate a random BST, use a built in traversal algorithm, and visualize the traversal process.
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,32 @@
 const path = require('path');
+
+module.exports = {
+  mode : process.env.NODE_ENV,
+  entry : path.resolve(__dirname, 'client/index.js'),
+  devServer : {
+    publicPath : '/build/',
+  },
+  output : {
+    path : path.resolve(__dirname, 'build'),
+    filename : 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /(node_modules)/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+}
